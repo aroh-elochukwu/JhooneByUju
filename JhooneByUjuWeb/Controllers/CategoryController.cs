5dabf0b6d99ef29a97fa3d7245820bcc5bf04043
Elochukwu Aroh <elochukwuaroh@gmail.com>
2022-09-19 16:54:32 -0500

    Add project files.

---
+++ JhooneByUjuWeb/Controllers/CategoryController.cs
@@ -0,0 +1,53 @@
+﻿using JhooneByUjuWeb.Data;
+using JhooneByUjuWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JhooneByUjuWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categoriesList = _db.Categories;
+
+            return View(categoriesList);
+        }
+
+        //GET
+        [HttpGet]       
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Id,Name,DisplayOrder,CreatedDateTIme")] Category category)
+        {
+            if(category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot be exactly the same as Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+
+            }
+            return View(category);
+            
+        }
+        
+    }
+}
---
+++ JhooneByUjuWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using JhooneByUjuWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JhooneByUjuWeb.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        
+    }
+}
