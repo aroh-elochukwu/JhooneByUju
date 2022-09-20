a9e8d72ec40fd7448079c99506885333a675dbe4
Elochukwu Aroh <elochukwuaroh@gmail.com>
2022-09-20 07:49:20 -0500

    repository attempt with uncomprehended issues while initiating ICategoryRepository (to revisit)

---
+++ JhooneByUju.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using JhooneByUju.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JhooneByUju.DataAccess.Repository.IRepository
+{
+    public class ICategoryRepository 
+    {
+        
+
+    }
+}
