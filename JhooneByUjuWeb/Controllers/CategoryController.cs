973ac67c991154ac28c8c5f94ec87ba03a23af55
Elochukwu Aroh <elochukwuaroh@gmail.com>
2022-09-19 20:31:03 -0500

    add category model, controller and views

--- JhooneByUjuWeb/Controllers/CategoryController.cs
+++ JhooneByUjuWeb/Controllers/CategoryController.cs
@@ -41,6 +41,7 @@ namespace JhooneByUjuWeb.Controllers
             {
                 _db.Categories.Add(category);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
 
                 return RedirectToAction("Index");
 
@@ -48,6 +49,70 @@ namespace JhooneByUjuWeb.Controllers
             return View(category);
             
         }
-        
+
+        //GET
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category requestedCategory = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+            return View(requestedCategory);
+        }
+
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([Bind("Id,Name,DisplayOrder,CreatedDateTIme")] Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot be exactly the same as Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category edited successfully";
+
+                return RedirectToAction("Index");
+
+            }
+            return View(category);
+
+        }
+
+        //GET
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category requestedCategory = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+            return View(requestedCategory);
+        }
+
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete([Bind("Id,Name,DisplayOrder,CreatedDateTIme")] Category category)
+        {
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- JhooneByUjuWeb/Migrations/ApplicationDbContextModelSnapshot.cs
+++ JhooneByUjuWeb/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -42,7 +42,7 @@ namespace JhooneByUjuWeb.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Categories");
+                    b.ToTable("Categories", (string)null);
                 });
 #pragma warning restore 612, 618
         }
